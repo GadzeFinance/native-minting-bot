a40b8f31b08f56acda7a658aa9a38f9e98f82b3a
Jacob Firek <firekjt@gmail.com>
2024-05-01 21:59:05 -0500

    feat: pagination filter

--- withdrawFilter.js
+++ withdrawFilter.js
@@ -1,13 +1,7 @@
 require('dotenv').config();
 
 const ethers = require('ethers');
-const provider = new ethers.providers.JsonRpcProvider("https://rpc.ankr.com/blast");
-
-provider.getBlockNumber().then((blockNumber) => {
-    console.log(`Current block number: ${blockNumber}`);
-}).catch((error) => {
-    console.error("Error fetching current block number:", error);
-});
+const provider = new ethers.providers.JsonRpcProvider("https://blast-mainnet.infura.io/v3/3cfca4bf32d54476ae33585ba8983c52");
 
 const blastL2MessengerABI = require('./L1CrossDomainMessagerABI.json');
 const blastL2MessengerAddress = '0x4200000000000000000000000000000000000007'; 
@@ -15,16 +9,45 @@ const blastL2MessengerAddress = '0x4200000000000000000000000000000000000007';
 // Create a contract instance
 const contract = new ethers.Contract(blastL2MessengerAddress, blastL2MessengerABI, provider);
 
-const filter = contract.filters.SentMessageExtension1();
+const startBlockInitial = 2612000;
+const blockInterval = 50000;
 
-provider.getLogs(filter).then((logs) => {
-    console.log(`Found ${logs.length} logs`);
-    logs.forEach((log) => {
-        const event = contract.interface.parseLog(log);
-        console.log(`Transaction Hash: ${log.transactionHash}`);
-        console.log(`Sender: ${event.args.sender}`);
-        console.log(`Value: ${event.args.value.toString()}`);
-    });
+provider.getBlockNumber().then(async (latestBlockNumber) => {
+    for (let startBlock = startBlockInitial; startBlock <= latestBlockNumber; startBlock += blockInterval) {
+        let endBlock = startBlock + blockInterval - 1;
+        
+        // Adjust the end block to not exceed the latest block number
+        if (endBlock > latestBlockNumber) {
+            endBlock = latestBlockNumber;
+        }
+
+        console.log(contract.filters.SentMessageExtension1().topics);
+
+        const filter = {
+            address: blastL2MessengerAddress,
+            topics: [
+                contract.filters.SentMessageExtension1().topics[0],
+                
+            ],
+            fromBlock: startBlock,
+            toBlock: endBlock
+        };
+
+        try {
+            const logs = await provider.getLogs(filter);
+            console.log(`Found ${logs.length} logs in block range ${startBlock} - ${endBlock}`);
+            logs.forEach((log) => {
+                const event = contract.interface.parseLog(log);
+
+                console.log(`Transaction Hash: ${log.transactionHash}`);
+                console.log(`Sender: ${event.args.sender}`);
+                console.log(`Value: ${event.args.value.toString()}`);
+
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    }
 }).catch((error) => {
     console.error(error);
 });
